c55472aae5cce1f556218866ac9c6efd1976c097
Tami Takamiya <ttakamiy@redhat.com>
2025-07-23 13:51:55 -0400

    generate inventory

--- ai_installer_template/server.py
+++ ai_installer_template/server.py
@@ -1,6 +1,8 @@
 # server.py
 import importlib.resources
 from fastmcp import FastMCP
+from aap_inventory_tool import generate_command
+
 mcp = FastMCP("AI Installer Template MCP Server")
 
 @mcp.tool
@@ -9,6 +11,22 @@ def get_template(platform: str, topology: str, session_id: str) -> str:
     with importlib.resources.open_text("ai_installer_template.data", f'{platform}_{topology}_template.txt') as f:
         content = f.read()
     return content
+
+@mcp.tool
+def get_inventory(platform: str, topology: str, host: str, session_id: str) -> str:
+    """Get an inventory for AAP Installer for given platform and topology"""
+    class Args:
+        platform: str
+        topology: str
+        host: str
+
+    args = Args()
+    args.platform = platform
+    args.topology = topology
+    args.host = host
+    print(f"platform:{platform} topology:{topology} session_id:{session_id}")
+
+    return generate_command(args)
 
 if __name__ == "__main__":
     mcp.run(transport="http")
